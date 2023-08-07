3ba4ae653b65d443d6e73a8b00b98eeacf6c18d7
Karan <karan@8848digital.com>
2023-08-07 19:13:07 +0530

    changes done for quick order page, update cart and multi-lingual

--- hooks/order-listing-page-hook/cart-order-history-hook.ts
+++ hooks/order-listing-page-hook/cart-order-history-hook.ts
@@ -4,14 +4,30 @@ import {
   FetchOrderListing,
   order_listing_state,
 } from "../../store/slices/order-listing-page-slice/order-listing-page-slice";
+import { get_access_token } from "../../store/slices/auth/token-login-slice";
 
 const UseCartOrderHistory = () => {
+  const dispatch = useDispatch();
   const [orderHistoryItems, setOrderHistoryItems] = useState<any>([]);
 
   const OrderListingProducts = useSelector(order_listing_state);
+  const TokenFromStore: any = useSelector(get_access_token);
 
   console.log("orderlisting data from store", OrderListingProducts);
+  const [history, setHistory] = useState("this_month");
+  const handleHistoryDate = (e: any) => {
+    setHistory(e.target.value);
+  };
 
+  useEffect(() => {
+    const reqParams = {
+      date: history,
+      id: "",
+      token: TokenFromStore?.token,
+    };
+    dispatch(FetchOrderListing(reqParams));
+    // dispatch(FetchOrderListing(history, "", TokenFromStore?.token));
+  }, [history]);
   // useEffect(()=>{
   //     dispatch(FetchOrderListing("this_month"))
   // },[])
@@ -27,6 +43,8 @@ const UseCartOrderHistory = () => {
   console.log("orderlisting hook end", orderHistoryItems);
   return {
     orderHistoryItems,
+    handleHistoryDate,
+    history,
   };
 };
 
