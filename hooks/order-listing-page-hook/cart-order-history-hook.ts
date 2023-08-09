d861006bede8e86f83cc90487cdd86555ae3a204
rushikesh2404 <141728045+rushikesh2404@users.noreply.github.com>
2023-08-09 11:47:10 +0530

    Update cart-order-history-hook.ts
    order history hook changes

--- hooks/order-listing-page-hook/cart-order-history-hook.ts
+++ hooks/order-listing-page-hook/cart-order-history-hook.ts
@@ -1,32 +1,73 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   FetchOrderListing,
   order_listing_state,
 } from "../../store/slices/order-listing-page-slice/order-listing-page-slice";
+import { get_access_token } from "../../store/slices/auth/token-login-slice";
 
 const UseCartOrderHistory = () => {
+  const dispatch = useDispatch();
   const [orderHistoryItems, setOrderHistoryItems] = useState<any>([]);
+  const [loadingStatus, setLoadingStatus] = useState<boolean>(false);
 
   const OrderListingProducts = useSelector(order_listing_state);
+  const TokenFromStore: any = useSelector(get_access_token);
 
   console.log("orderlisting data from store", OrderListingProducts);
+  const [history, setHistory] = useState("this_month");
+  const handleHistoryDate = (e: any) => {
+    setHistory(e.target.value);
+  };
 
-  // useEffect(()=>{
-  //     dispatch(FetchOrderListing("this_month"))
-  // },[])
+  useEffect(() => {
+    const reqParams = {
+      date: history,
+      id: "",
+      token: TokenFromStore?.token,
+    };
+    dispatch(FetchOrderListing(reqParams));
+    // dispatch(FetchOrderListing(history, "", TokenFromStore?.token));
+  }, [history]);
 
   useEffect(() => {
     if (OrderListingProducts?.data?.length > 0) {
       setOrderHistoryItems(OrderListingProducts.data);
+      setLoadingStatus(false);
     } else {
       setOrderHistoryItems([]);
     }
   }, [OrderListingProducts]);
 
-  console.log("orderlisting hook end", orderHistoryItems);
+  useEffect(() => {
+    setLoadingStatus(true);
+    if (
+      OrderListingProducts?.data?.length < 0 &&
+      OrderListingProducts?.isLoading === "pending"
+    ) {
+      setLoadingStatus(true);
+    } else if (
+      OrderListingProducts?.data?.length > 0 &&
+      OrderListingProducts?.isLoading === "succeeded"
+    ) {
+      setOrderHistoryItems(OrderListingProducts?.data);
+      setLoadingStatus(false);
+    } else if (
+      OrderListingProducts?.data?.length < 0 &&
+      OrderListingProducts?.isLoading === "succeeded"
+    ) {
+      setLoadingStatus(false);
+      setOrderHistoryItems([]);
+    } else if (OrderListingProducts?.isLoading === "succeeded") {
+      setLoadingStatus(false);
+    }
+  }, [OrderListingProducts]);
+
   return {
     orderHistoryItems,
+    handleHistoryDate,
+    history,
+    loadingStatus,
   };
 };
 
