db9b3f2d0c2a0aefb7908acc32cb62d73777fdaf
Karan <123349027+karan1633@users.noreply.github.com>
2023-09-05 13:46:33 +0530

    changes

--- store/slices/general_slices/cache-slice.ts
+++ store/slices/general_slices/cache-slice.ts
@@ -2,23 +2,22 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../root-reducer";
 
 const initialState = {
-    cacheData:{}
+  cacheData: {},
 };
 
 const cacheSlice = createSlice({
-    name:"cache-slice",
-    initialState,
-    reducers:{
-        setCacheData(state, action)
-        {
-            state.cacheData = action.payload
-        }
-    }
+  name: "cache-slice",
+  initialState,
+  reducers: {
+    setRevalidationTime(state, action) {
+      //   console.log("validation time", action);
+      state.cacheData = action.payload;
+    },
+  },
 });
 
-export const {setCacheData} = cacheSlice.actions;
+export const { setRevalidationTime } = cacheSlice.actions;
 
-export const cache_data_state = (state: RootState) =>
-  state.CacheScreen;
+export const cache_data_state = (state: RootState) => state.CacheScreen;
 
 export default cacheSlice.reducer;
