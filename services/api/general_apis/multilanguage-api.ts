bf8cf5826ac2c3c85f518d76b105c212ada4a572
Rushiahire <rushiahire9567@gmail.com>
2023-09-13 16:39:15 +0530

    quick order,search hook and multilang api changes

--- services/api/general_apis/multilanguage-api.ts
+++ services/api/general_apis/multilanguage-api.ts
@@ -33,33 +33,35 @@ const MultiLangApi = async () => {
   const langTransmethod = "get_translation_text";
   const langTransentity = "translation";
 
-  const generateMultiLingualArrayOfData = await Promise.all(
-    MultiLanguagesList.map(async (lang: any) => {
-      console.log("MultiLanguagesList langname", lang);
-      try {
-        const res = await axios.get(
-          `${CONSTANTS.API_BASE_URL}${CONSTANTS.API_MANDATE_PARAMS}?version=${version}&method=${langTransmethod}&entity=${langTransentity}&language_code=${lang.language_code}`,
-          { timeout: 5000 }
-        );
+  const generateMultiLingualArrayOfData =
+    MultiLanguagesList?.length > 0 &&
+    (await Promise.all(
+      MultiLanguagesList.map(async (lang: any) => {
+        console.log("MultiLanguagesList langname", lang);
+        try {
+          const res = await axios.get(
+            `${CONSTANTS.API_BASE_URL}${CONSTANTS.API_MANDATE_PARAMS}?version=${version}&method=${langTransmethod}&entity=${langTransentity}&language_code=${lang.language_code}`,
+            { timeout: 5000 }
+          );
 
-        return {
-          lang_name: lang.language_name,
-          lang_code: lang.language_code,
-          value: res?.data?.message,
-        };
-      } catch (err: any) {
-        if (err.code === "ECONNABORTED") {
-          response = "Request timed out";
-        } else if (err.code === "ERR_BAD_REQUEST") {
-          response = "Bad Request";
-        } else if (err.code === "ERR_INVALID_URL") {
-          response = "Invalid URL";
-        } else {
-          response = err;
+          return {
+            lang_name: lang.language_name,
+            lang_code: lang.language_code,
+            value: res?.data?.message,
+          };
+        } catch (err: any) {
+          if (err.code === "ECONNABORTED") {
+            response = "Request timed out";
+          } else if (err.code === "ERR_BAD_REQUEST") {
+            response = "Bad Request";
+          } else if (err.code === "ERR_INVALID_URL") {
+            response = "Invalid URL";
+          } else {
+            response = err;
+          }
         }
-      }
-    })
-  );
+      })
+    ));
 
   return generateMultiLingualArrayOfData;
 };
