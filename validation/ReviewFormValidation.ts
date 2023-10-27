30a55f659de30f15dd2f51e1ab0d9aa1d36b7feb
8848digital <ritik@8848digital.com>
2023-10-27 11:05:56 +0530

    validation in Review:RV

---
+++ validation/ReviewFormValidation.ts
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+const ReviewFormValidation = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    images: Yup.mixed().required('Image is required'),
+    description: Yup.string().required('Description is required'),
+});
+
+export default ReviewFormValidation
