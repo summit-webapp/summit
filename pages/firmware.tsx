705a45e53e85d6d230ef9e604aa66427a7e5fc98
unknown <shadab@8848digital.com>
2023-12-01 10:35:17 +0530

    warranty page added:sk

---
+++ pages/firmware.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FirmwareComponent from '../components/Firmware/Firmware'
+
+const index = () => {
+  return (
+    <div><FirmwareComponent/></div>
+  )
+}
+
+export default index
