197d7ceea8753a0e0a36e573e7f5f7675bec9f24
rushikesh2404 <rushikesh@8848digital.com>
2024-07-05 18:36:43 +0530

    display tag changes

--- services/api/home_page_api/home-display-tag-api.ts
+++ services/api/home_page_api/home-display-tag-api.ts
@@ -6,22 +6,26 @@ const getDisplaytagsDataFromAPI = async (token: any, currencyValue: any) => {
   const method = 'get_tagged_products';
   const entity = 'product';
   const params = `?version=${version}&method=${method}&entity=${entity}`;
+  console.log('token', token);
+  const displayTagsList = await callGetAPI(`${CONSTANTS.API_BASE_URL}/api/resource/Tag`, token);
 
-  const displayTagsList = await callGetAPI(`${CONSTANTS.API_BASE_URL}/api/resource/Tag${params}`, token);
+  if (displayTagsList?.data?.data?.length > 0) {
+    const getDisplayTagsProductsList: any = await Promise.all(
+      displayTagsList?.data?.data?.length > 0 &&
+        displayTagsList?.data?.data.map(async (tag: any) => {
+          const res = await callGetAPI(
+            `${CONSTANTS.API_BASE_URL}/${CONSTANTS.API_MANDATE_PARAMS}${params}&tag=${tag.name}&currency=${currencyValue}`,
+            token
+          );
 
-  const getDisplayTagsProductsList: any = await Promise.all(
-    displayTagsList?.data?.data?.length > 0 &&
-      displayTagsList?.data?.data.map(async (tag: any) => {
-        const res = await callGetAPI(
-          `${CONSTANTS.API_BASE_URL}/${CONSTANTS.API_MANDATE_PARAMS}${params}&tag=${tag.name}&currency=${currencyValue}`,
-          token
-        );
+          return { tag_name: tag.name, value: res?.data?.message?.data };
+        })
+    );
 
-        return { tag_name: tag.name, value: res?.data?.message?.data };
-      })
-  );
-
-  return getDisplayTagsProductsList;
+    return getDisplayTagsProductsList;
+  } else {
+    return displayTagsList;
+  }
 };
 
 export default getDisplaytagsDataFromAPI;
