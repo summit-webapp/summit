a6a7fbe1a5fa395b358c66c9409600ffcfdca9ae
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-07-09 12:23:10 +0530

    feat:navigate to product listing from navbar added

--- pages/product-category/index.tsx
+++ pages/product-category/index.tsx
@@ -1,14 +1,14 @@
 import { version } from 'os';
-import Header from '../../components/Header/Header';
-import ProductListingMaster from '../../components/ProductListingComponents/ProductListingMaster';
 import MetaTag from '../../services/api/general_apis/meta-tag-api';
 import { CONSTANTS } from '../../services/config/app-config';
+import ProductListingMaster from '../../components/ProductListingComponents/ProductListingMaster';
+import PageMetaData from '../../components/PageMetaData/PageMetaData';
 
 const Index = ({ metaData }: any) => {
   console.log('meta data in page', metaData);
   return (
     <>
-      {CONSTANTS.ENABLE_META_TAGS && <Header meta_data={metaData} />}
+      {CONSTANTS.ENABLE_META_TAGS && <PageMetaData meta_data={metaData} />}
       <>
         <ProductListingMaster />
       </>
