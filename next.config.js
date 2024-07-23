934efb9ef4ce3535eb80c76253fe19cbb9e1e06c
karan <karan@8848digital.com>
2024-07-23 17:35:38 +0530

    feat: optimization changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
