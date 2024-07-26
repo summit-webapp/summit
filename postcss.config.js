c90fd63cfe2fa07cbca59ac80e0171b1f8a2b918
monika8848 <monika@8848digital.com>
2024-07-26 17:41:27 +0530

    feat: changes in banner hook file

--- postcss.config.js
+++ postcss.config.js
@@ -1,42 +1,42 @@
 module.exports = {
   plugins: [
-    // 'postcss-flexbugs-fixes',
-    // [
-    //   'postcss-preset-env',
-    //   {
-    //     autoprefixer: {
-    //       flexbox: 'no-2009',
-    //     },
-    //     stage: 3,
-    //     features: {
-    //       customProperties: false,
-    //     },
-    //   },
-    // ],
-    // [
-    //   '@fullhuman/postcss-purgecss',
-    //   {
-    //     content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './styles/**/*.{scss,css}'],
-    //     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    //     safelist: [
-    //       'html',
-    //       'body',
-    //       'slick-slider',
-    //       'slick-list',
-    //       'slick-track',
-    //       'slick-slide',
-    //       'slick-active',
-    //       'slick-initialized',
-    //       'slick-loading',
-    //       'slick-dots',
-    //     ],
-    //   },
-    // ],
-    // [
-    //   'cssnano',
-    //   {
-    //     preset: 'default',
-    //   },
-    // ],
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          customProperties: false,
+        },
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './styles/**/*.{scss,css}'],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: [
+          'html',
+          'body',
+          'slick-slider',
+          'slick-list',
+          'slick-track',
+          'slick-slide',
+          'slick-active',
+          'slick-initialized',
+          'slick-loading',
+          'slick-dots',
+        ],
+      },
+    ],
+    [
+      'cssnano',
+      {
+        preset: 'default',
+      },
+    ],
   ],
 };
