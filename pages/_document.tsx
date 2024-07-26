4b5cc6249b233b84b6dfd6d77d888dfe83f6382c
monika8848 <monika@8848digital.com>
2024-07-26 16:10:39 +0530

    feat: changes in banner hook file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,18 +4,7 @@ import { CONSTANTS } from '../services/config/app-config';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="preconnect" href={`${CONSTANTS.API_BASE_URL}`} />
-        {/* <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          onLoad={(e) => {
-            const target = e.target as HTMLLinkElement;
-            target.media = 'all';
-          }}
-        ></link>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" async></script> */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
