11fb21a1ba50021529dcffff16e574d6b1148c98
monika8848 <monika@8848digital.com>
2024-07-26 16:11:47 +0530

    feat: changes in banner hook file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ import { CONSTANTS } from '../services/config/app-config';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href={`${CONSTANTS.API_BASE_URL}`} />
+      </Head>
       <body>
         <Main />
         <NextScript />
