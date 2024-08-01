df12dff99de2e0a8da8a93a289a180cf6101c03f
rushikesh2404 <rushikesh@8848digital.com>
2024-08-01 17:12:20 +0530

    fix: add-to-cart api new changes

--- services/api/cart-apis/add-to-cart-api.ts
+++ services/api/cart-apis/add-to-cart-api.ts
@@ -2,15 +2,10 @@ import axios from 'axios';
 import { CONSTANTS } from '../../config/app-config';
 import { callPostAPI } from '../../../utils/utils';
 
-<<<<<<< HEAD:services/api/cart-page-api/add-to-cart-api.ts
-const AddToCartPostApi: any = async (params:any, token?: any) => {
-  let version = CONSTANTS.VERSION;
-=======
 export const PostAddToCartAPI: any = async (apiBody: any, token?: any) => {
   console.log('add currency in api', token);
   let response: any;
   let version = CONSTANTS.CUSTOM_API_SDK_VERSION;
->>>>>>> 9914735fe90ff98e90b07f6c4075ffe4ca9de7e3:services/api/cart-apis/add-to-cart-api.ts
   const method = 'put_products';
   const entity = 'cart';
   const apiSDKName = CONSTANTS.CUSTOM_API_SDK;
@@ -19,22 +14,6 @@ export const PostAddToCartAPI: any = async (apiBody: any, token?: any) => {
     version: version,
     method: method,
     entity: entity,
-<<<<<<< HEAD:services/api/cart-page-api/add-to-cart-api.ts
-    item_code: params?.item_code,
-    party_name: params?.party_name,
-    purity: params?.purity,
-    cust_name: params?.cust_name,
-    colour: params?.colour,
-    wastage: params?.wastage,
-    qty_size_list: params?.qty_size_list,
-    remark: params?.remark,
-    user: params?.user,
-  };
-
-  const url = `${CONSTANTS.API_BASE_URL}/${CONSTANTS.API_MANDATE_PARAMS}`;
-
-  const response = await callPostAPI(url, body, token);
-=======
     ...apiBody,
   };
 
@@ -57,7 +36,6 @@ export const PostAddToCartAPI: any = async (apiBody: any, token?: any) => {
         response = err;
       }
     });
->>>>>>> 9914735fe90ff98e90b07f6c4075ffe4ca9de7e3:services/api/cart-apis/add-to-cart-api.ts
   return response;
 };
 
@@ -103,10 +81,3 @@ export const PostQuickOrderAddToCart = async (item_data: any) => {
     });
   return response;
 };
-<<<<<<< HEAD:services/api/cart-page-api/add-to-cart-api.ts
-
-const AddToCartApi = (params:any, token?: any) => AddToCartPostApi(params,token);
-
-export default AddToCartApi;
-=======
->>>>>>> 9914735fe90ff98e90b07f6c4075ffe4ca9de7e3:services/api/cart-apis/add-to-cart-api.ts
