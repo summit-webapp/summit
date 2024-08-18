f6fdf47870d2487589807477aa23b7a2089fcaf6
karan <karan.tamboli2001@gmail.com>
2024-08-18 14:42:30 +0530

    feat: on successfully registration redirect to login page :k

--- hooks/AuthHooks/useRegisterUser.ts
+++ hooks/AuthHooks/useRegisterUser.ts
@@ -4,9 +4,11 @@ import typeRegistrationProps from '../../interfaces/registration-interface';
 import registrationAPI from '../../services/api/auth/registration-api';
 import { CONSTANTS } from '../../services/config/app-config';
 import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
 const useRegisterUser = () => {
   const { SUMMIT_APP_CONFIG } = CONSTANTS;
+  const router = useRouter();
   const { isLoading, setIsLoading, errorMessage, setErrMessage } = useHandleStateUpdate();
   const [registrationFormData, setRegistrationFormData] = useState<typeRegistrationProps>({
     salutation: '',
@@ -42,7 +44,10 @@ const useRegisterUser = () => {
     try {
       registrationAPIResponse = await registrationAPI(SUMMIT_APP_CONFIG, cleanedData);
       if (registrationAPIResponse?.status === 200 && registrationAPIResponse?.data?.message?.msg === 'success') {
-        toast.success('User Registered successfully!');
+        toast.success('User Registered successfully! Please login');
+        setTimeout(() => {
+          router.push('/login');
+        }, 1500);
       }
     } catch (error) {
       setErrMessage(registrationAPIResponse?.data?.message?.error);
