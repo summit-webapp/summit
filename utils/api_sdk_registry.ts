178b15087ca4e472aeb5cb59fa451fb60e45e8b3
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-08-20 17:13:27 +0530

    fix: version method entity added

--- utils/api_sdk_registry.ts
+++ utils/api_sdk_registry.ts
@@ -1,22 +1,18 @@
-import { CONSTANTS } from '../services/config/app-config';
-const { CUSTOM_API_SDK } = CONSTANTS;
-
 const apiSdkRegistry: any = {
   'registration-api': { version: '', method: 'customer_signup', entity: 'registration' },
-  'navbar-api': '',
-  'banner-api': '',
-  'display-tags-api': '',
-  'breadcrums-api': '',
-  'get-listing-filters-api': '',
-  'product-list-api': '',
-  'product-detail-api': '',
-  'product-variants-api': '',
-  'wishlist-api': '',
-  'cart-list-api': CUSTOM_API_SDK,
-  'order-list-api': CUSTOM_API_SDK,
-  'order-detail-api': CUSTOM_API_SDK,
-  'order-reports-api': CUSTOM_API_SDK,
-  'customer-item-api': CUSTOM_API_SDK,
+  'navbar-api': { version: '', method: 'get_mega_menu', entity: 'mega_menu' },
+  'banner-api': { version: '', method: 'get', entity: 'banner' },
+  'display-tags-api': { version: '', method: 'get_tagged_products', entity: 'product' },
+  'breadcrums-api': { version: '', method: 'breadcrums', entity: 'mega_menu' },
+  'get-listing-filters-api': { version: '', method: 'get_filters', entity: 'filter' },
+  'product-list-api': { version: '', method: 'get_list', entity: 'product' },
+  'product-detail-api': { version: '', method: 'get_details', entity: 'product' },
+  'product-variants-api': { version: '', method: 'get_variants', entity: 'variant' },
+  'wishlist-api': { version: '', method: 'get_wishlist_items', entity: 'wishlist' },
+  'cart-list-api': { version: '', method: 'get_list', entity: 'cart' },
+  'order-list-api': { version: '', method: 'get_list', entity: 'order' },
+  'order-detail-api': { version: '', method: 'get_order_details', entity: 'order' },
+  'order-reports-api': { version: '', method: '', entity: '' },
 };
 
 export default apiSdkRegistry;
