ddd1294739c3211368bdbbfca547875bf131cd19
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-09-11 14:02:51 +0530

    fix: order page added

---
+++ pages/MyOrder/index.tsx
@@ -0,0 +1,35 @@
+import OrderMaster from "../../components/MyOrder/OrderMaster";
+import MetaTag from "../../services/api/general-apis/meta-tag-api";
+import { CONSTANTS } from "../../services/config/app-config";
+
+const MyOrder = () => {
+    return (
+        <>
+            <OrderMaster />
+        </>
+    );
+};
+
+export async function getServerSideProps(context: any) {
+    const { SUMMIT_APP_CONFIG } = CONSTANTS;
+    const method = 'get_meta_tags';
+    const version = SUMMIT_APP_CONFIG.version;
+    const entity = 'seo';
+    const params = `?version=${version}&method=${method}&entity=${entity}`;
+    const url = `${context.resolvedUrl.split('?')[0]}`;
+
+    if (CONSTANTS.ENABLE_META_TAGS) {
+        let meta_data: any = await MetaTag(`${CONSTANTS.API_BASE_URL}${SUMMIT_APP_CONFIG.app_name}${params}&page_name=${url}`);
+
+        if (meta_data !== null && Object.keys(meta_data).length > 0) {
+            const metaData = meta_data?.data?.message?.data;
+            return { props: { metaData } };
+        } else {
+            return { props: {} };
+        }
+    } else {
+        return { props: {} };
+    }
+}
+
+export default MyOrder;
