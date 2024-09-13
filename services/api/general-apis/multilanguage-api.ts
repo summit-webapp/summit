72d10072487f71617a34a049b6aea1cb50ac8306
karan <karan@8848digital.com>
2024-09-13 19:14:03 +0530

    feat: :mag: SEO changes in pages directory

--- services/api/general-apis/multilanguage-api.ts
+++ services/api/general-apis/multilanguage-api.ts
@@ -15,35 +15,38 @@ const MultiLangApi = async (appConfig: APP_CONFIG) => {
   );
   const langTransmethod = 'get_translation_text';
   const langTransentity = 'translation';
+  let generateMultiLingualArrayOfData: any = [];
+  if (MultiLanguagesList?.status === 200 && Array.isArray(MultiLanguagesList?.data?.message)) {
+    generateMultiLingualArrayOfData =
+      MultiLanguagesList?.data?.message?.length > 0
+        ? await Promise.all(
+            MultiLanguagesList?.data?.message.map(async (lang: any) => {
+              try {
+                const res = await axios.get(
+                  `${CONSTANTS.API_BASE_URL}${CONSTANTS.SUMMIT_APP_CONFIG?.app_name}?version=${CONSTANTS.SUMMIT_APP_CONFIG.version}&method=${langTransmethod}&entity=${langTransentity}&language_code=${lang.language_code}`,
+                  { timeout: 5000 }
+                );
 
-  const generateMultiLingualArrayOfData =
-    MultiLanguagesList?.data?.message?.length > 0 &&
-    (await Promise.all(
-      MultiLanguagesList?.data?.message.map(async (lang: any) => {
-        try {
-          const res = await axios.get(
-            `${CONSTANTS.API_BASE_URL}${CONSTANTS.SUMMIT_APP_CONFIG?.app_name}?version=${CONSTANTS.SUMMIT_APP_CONFIG.version}&method=${langTransmethod}&entity=${langTransentity}&language_code=${lang.language_code}`,
-            { timeout: 5000 }
-          );
-
-          return {
-            lang_name: lang.language_name,
-            lang_code: lang.language_code,
-            value: res?.data?.message,
-          };
-        } catch (err: any) {
-          if (err.code === 'ECONNABORTED') {
-            response = 'Request timed out';
-          } else if (err.code === 'ERR_BAD_REQUEST') {
-            response = 'Bad Request';
-          } else if (err.code === 'ERR_INVALID_URL') {
-            response = 'Invalid URL';
-          } else {
-            response = err;
-          }
-        }
-      })
-    ));
+                return {
+                  lang_name: lang.language_name,
+                  lang_code: lang.language_code,
+                  value: res?.data?.message,
+                };
+              } catch (err: any) {
+                if (err.code === 'ECONNABORTED') {
+                  response = 'Request timed out';
+                } else if (err.code === 'ERR_BAD_REQUEST') {
+                  response = 'Bad Request';
+                } else if (err.code === 'ERR_INVALID_URL') {
+                  response = 'Invalid URL';
+                } else {
+                  response = err;
+                }
+              }
+            })
+          )
+        : [];
+  }
 
   return generateMultiLingualArrayOfData;
 };
