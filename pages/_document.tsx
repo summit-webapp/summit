65993dde8fd13d423ba816ba84fb64357646e136
karan <karan.tamboli2001@gmail.com>
2024-09-30 12:55:31 +0530

    feat: :sparkles: google tag manager setup. tag manager script added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { CONSTANTS } from '../services/config/app-config';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="preconnect" href={`${CONSTANTS.API_BASE_URL}`} />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X9PK6K9PE5"></Script>
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', 'G-X9PK6K9PE5')`}
+        </Script>
       </Head>
       <body>
         <Main />
