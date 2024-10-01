67d4f28b6282fdea1efb3e6f892974d91e27a94b
karan <karan.tamboli2001@gmail.com>
2024-10-01 15:27:37 +0530

    fix: :chart_with_upwards_trend: updated ga4 import statement in hook files

--- hooks/GoogleAnalytics/useGoogleAnalyticsHandler.ts
+++ hooks/GoogleAnalytics/useGoogleAnalyticsHandler.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const useGoogleAnalyticsHandler = () => {
   const TRACKING_ID: string | undefined = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_CODE;
