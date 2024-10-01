b56ee4fe3e1ce539131d4fac4f2d9123d38ec493
karan <karan.tamboli2001@gmail.com>
2024-10-01 14:08:50 +0530

    feat: :sparkles: GA hook file added

---
+++ hooks/GoogleAnalytics/useGoogleAnalyticsHandler.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+
+const useGoogleAnalyticsHandler = () => {
+  const TRACKING_ID: string | undefined = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_CODE;
+  if (TRACKING_ID) {
+    ReactGA.initialize(TRACKING_ID);
+  } else {
+    console.error('You have enabled Google Analytics but tracking ID is not specified. Please add Tracking ID!');
+  }
+  return {};
+};
+
+export default useGoogleAnalyticsHandler;
