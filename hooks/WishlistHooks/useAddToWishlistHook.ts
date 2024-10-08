020a34cf515396805b9b55062e97a943e0624e21
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-10-08 16:06:16 +0530

    fix: wishlist success toast notification removed

--- hooks/WishlistHooks/useAddToWishlistHook.ts
+++ hooks/WishlistHooks/useAddToWishlistHook.ts
@@ -1,10 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
-import { get_access_token } from '../../store/slices/auth/token-login-slice';
 import { AddProductToWishlist, DeleteProductFromWishlist } from '../../services/api/wishlist-apis/wishlist-api';
-import { addItemToWishlist, removeItemFromWishlist } from '../../store/slices/wishlist-slices/wishlist-local-slice';
 import { CONSTANTS } from '../../services/config/app-config';
-import APP_CONFIG from '../../interfaces/app-config-interface';
+import { get_access_token } from '../../store/slices/auth/token-login-slice';
+import { addItemToWishlist, removeItemFromWishlist } from '../../store/slices/wishlist-slices/wishlist-local-slice';
 
 const useAddToWishlist = () => {
   const { SUMMIT_APP_CONFIG } = CONSTANTS;
@@ -17,9 +16,6 @@ const useAddToWishlist = () => {
     const addItem = await AddProductToWishlist(SUMMIT_APP_CONFIG, params, TokenFromStore?.token);
     if (addItem?.data?.message?.msg === 'success') {
       dispatch(addItemToWishlist(item));
-      toast.success('Product added to wishlist successfully!');
-    } else {
-      toast.error('Faild to add product to wishlist');
     }
   };
   const handleRemoveFromWishList = async (item_code: any) => {
@@ -29,7 +25,6 @@ const useAddToWishlist = () => {
     const removeItem = await DeleteProductFromWishlist(SUMMIT_APP_CONFIG, params, TokenFromStore?.token);
     if (removeItem?.data?.message?.msg === 'success') {
       dispatch(removeItemFromWishlist(item_code));
-      toast.success('Product removed from wishlist successfully!');
     } else {
       toast.error('Failed to remove product from wishlist');
     }
