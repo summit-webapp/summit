7d8b63a45d2740549857da307b92013183ab225d
karan <karan.tamboli2001@gmail.com>
2024-11-21 16:34:17 +0530

    feat: :sparkles: components list api integration

--- services/api/home-page-apis/get-home-page-components.ts
+++ services/api/home-page-apis/get-home-page-components.ts
@@ -1,17 +1,17 @@
 import APP_CONFIG from '../../../interfaces/app-config-interface';
 import { executeGETAPI } from '../../../utils/http-methods';
 
-const getHomePageComponents = async (appConfig: APP_CONFIG) => {
+const getHomePageComponentsList = async (appConfig: APP_CONFIG) => {
   const additionalParams = {}; // Add additional parameters if needed
   // Use executeGETAPI to handle GET Request logic
   const response = await executeGETAPI(
     appConfig,
     'home-page-components-api',
-    token,
+    undefined,
     additionalParams // Pass additional parameters if needed
   );
 
   return response;
 };
 
-export default getBannerAPI;
+export default getHomePageComponentsList;
