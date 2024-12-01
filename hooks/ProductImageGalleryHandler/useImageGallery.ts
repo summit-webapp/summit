60eb0d980505bc6477ff431b5ef863753a26144f
karan <karan.tamboli2001@gmail.com>
2024-12-01 17:35:49 +0530

    refactor: :mute: remove logs

--- hooks/ProductImageGalleryHandler/useImageGallery.ts
+++ hooks/ProductImageGalleryHandler/useImageGallery.ts
@@ -2,13 +2,12 @@ import { useState } from 'react';
 import { CONSTANTS } from '../../services/config/app-config';
 
 const useImageGallery = ({ slideShowImages }: any) => {
-  console.log(slideShowImages);
   const [enlargeImg, setEnlargeImg] = useState<string>(slideShowImages?.[0] || ''); // Track the enlarged image
   const [activeImgIndex, setActiveImgIndex] = useState<number>(0); // Track the active thumbnail index
 
-  const handleSelectedImage = (image: string, i: number) => {
-    setEnlargeImg(image);
-    setActiveImgIndex(i); // Update the active index
+  const handleSelectedImage = (imageLink: string, imgIndex: number) => {
+    setEnlargeImg(imageLink);
+    setActiveImgIndex(imgIndex); // Update the active index
   };
 
   // Create srcSet for different screen resolutions
