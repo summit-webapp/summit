0bc98da4935abcfd32db93d6b3836ee9be941589
karan <karan.tamboli2001@gmail.com>
2024-12-10 13:32:18 +0530

    fix: :bug: dispatch action condition changed

--- hooks/GeneralHooks/useInitializeStoreWithComponentsList.tsx
+++ hooks/GeneralHooks/useInitializeStoreWithComponentsList.tsx
@@ -1,14 +1,14 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { componentsListFromReduxStore, createComponentsList } from '../../store/slices/general_slices/components-slice';
-import { ComponentsTypes } from '../../interfaces/meta-data-interface';
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ComponentsTypes } from '../../interfaces/meta-data-interface';
+import { componentsListFromReduxStore, createComponentsList } from '../../store/slices/general_slices/components-slice';
 
 const useInitializeStoreWithComponentsList = (componentsListData: ComponentsTypes[]) => {
   const dispatch = useDispatch();
   const { componentsList }: any = useSelector(componentsListFromReduxStore);
 
   useEffect(() => {
-    if (componentsList?.length !== 0) {
+    if (componentsList?.length === 0) {
       dispatch(createComponentsList(componentsListData));
     }
   }, []);
