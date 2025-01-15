9808ffafe03e149f1dd213a19b1268db8a5ad646
karan <karan@8848digital.com>
2025-01-15 13:10:15 +0530

    feat: :label: summit settings types added

---
+++ interfaces/summit-settings-interface.ts
@@ -0,0 +1,15 @@
+export interface SummitSettingsTypes {
+  name?: string;
+  owner?: string;
+  modified?: string;
+  modified_by?: string;
+  docstatus?: number;
+  idx?: number;
+  enable_user_based_menu?: number;
+  variant_type?: string;
+  header_component?: string;
+  show_variant_on_product_card?: number;
+  variant_attribute_on_product_card?: string;
+  footer_component?: string;
+  doctype?: string;
+}
