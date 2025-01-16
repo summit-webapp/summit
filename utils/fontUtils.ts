6eefc7c24cfce3f795e12e2598f314188a3e69e5
karan <karan@8848digital.com>
2025-01-16 13:45:37 +0530

    build: :green_heart: fixed build errors

--- utils/fontUtils.ts
+++ utils/fontUtils.ts
@@ -1,7 +1,7 @@
 import { Belgrano, Belleza, Cairo, Catamaran, Nunito, Playfair_Display, Poppins, Roboto, Sofia, Spectral } from 'next/font/google';
 
 // Lots of approaches can be use to use fonts dynamically. But below code approach is to fix error - Font loaders must be called and assigned
-// to a const in the module scope
+// to a const in the module scope on build.
 
 /**
  * Preload all fonts at the module level.
@@ -37,13 +37,18 @@ const fonts: any = {
   Sofia: sofiaFont,
   Spectral: spectralFont,
 } as const;
-
 /**
  * Export function to get the font by name.
  *
- * @param {string} fontName - The name of the font to import.
+ * @param {string} fontName - The name of the font to import. Spaces in the name will be replaced with underscores.
  * @returns {Object} - The imported font object with its className.
  */
-export const createFontImport = (fontName: any) => {
-  return fonts[fontName] || fonts.Nunito; // Default to Nunito if font is not found
+export const createFontImport = (fontName: string) => {
+  if (!fontName) return fonts.Nunito; // Default to Nunito if no fontName is provided
+
+  // Normalize fontName by replacing spaces with underscores
+  const normalizedFontName = fontName.replace(/ /g, '_');
+
+  // Check if the normalized name exists in the fonts object
+  return fonts[normalizedFontName as keyof typeof fonts] || fonts.Nunito;
 };
