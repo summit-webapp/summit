dfc11cd07f71df980d58fd4e0a31417cc54fe942
NikitaD8848 <nikita.dhumal@8848digital.com>
2025-01-23 18:03:49 +0530

    feat: promotional banner api function added

--- hooks/HomePageHooks/usePromotionalBanner.ts
+++ hooks/HomePageHooks/usePromotionalBanner.ts
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { CONSTANTS } from '../../services/config/app-config';
 import { get_access_token } from '../../store/slices/auth/token-login-slice';
 import useHandleStateUpdate from '../GeneralHooks/handle-state-update-hook';
+import promotionalBannerAPI from '../../services/api/home-page-apis/promotional-banner-api';
 const usePromotionalBanner = () => {
   const [promotionalBannerData, setPromotionalBannerData] = useState<any>([]);
   const tokenFromStore: any = useSelector(get_access_token);
@@ -10,11 +11,13 @@ const usePromotionalBanner = () => {
   const { SUMMIT_APP_CONFIG }: any = CONSTANTS;
   const fetchPromotionalBannerData = async () => {
     let getPromotionalBannerData: any;
+    const reqParams = ['title', 'description', 'image', 'sequence'];
+
     setIsLoading(true);
     try {
-      getPromotionalBannerData = await promotionalBannerData(SUMMIT_APP_CONFIG, tokenFromStore?.token);
-      if (getPromotionalBannerData?.status === 200 && getPromotionalBannerData?.data?.msg === 'success') {
-        setPromotionalBannerData(getPromotionalBannerData?.data?.data);
+      getPromotionalBannerData = await promotionalBannerAPI(SUMMIT_APP_CONFIG, reqParams, tokenFromStore?.token);
+      if (getPromotionalBannerData?.length > 0) {
+        setPromotionalBannerData(getPromotionalBannerData);
       } else {
         setErrMessage('No Data Found');
       }
