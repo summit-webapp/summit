7ae015895db98b43297084fa2caf9a0b01cd6977
monika8848 <monika@8848digital.com>
2025-01-27 14:10:17 +0530

    feat: change in promotonal banner api and hook

--- hooks/HomePageHooks/usePromotionalBanner.ts
+++ hooks/HomePageHooks/usePromotionalBanner.ts
@@ -3,17 +3,51 @@ import { useSelector } from 'react-redux';
 import { CONSTANTS } from '../../services/config/app-config';
 import { get_access_token } from '../../store/slices/auth/token-login-slice';
 import useHandleStateUpdate from '../GeneralHooks/handle-state-update-hook';
+import { currency_selector_state } from '../../store/slices/general_slices/multi-currency-slice';
+import promotionalBannerAPI from '../../services/api/home-page-apis/promotional-banner-api';
 const usePromotionalBanner = () => {
-  const [promotionalBannerData, setPromotionalBannerData] = useState<any>([]);
-  const tokenFromStore: any = useSelector(get_access_token);
+  // const [promotionalBannerData, setPromotionalBannerData] = useState<any>([]);
+  // const tokenFromStore: any = useSelector(get_access_token);
+  // const { isLoading, setIsLoading, errorMessage, setErrMessage }: any = useHandleStateUpdate();
+  // const { SUMMIT_APP_CONFIG }: any = CONSTANTS;
+  // const fetchPromotionalBannerData = async () => {
+  //   let getPromotionalBannerData: any;
+  //   setIsLoading(true);
+  //   try {
+  //     getPromotionalBannerData = await promotionalBannerData(SUMMIT_APP_CONFIG, tokenFromStore?.token);
+  //     if (getPromotionalBannerData?.status === 200 && getPromotionalBannerData?.data?.msg === 'success') {
+  //       setPromotionalBannerData(getPromotionalBannerData?.data?.data);
+  //     } else {
+  //       setErrMessage('No Data Found');
+  //     }
+  //   } catch (error) {
+  //     setErrMessage('No Data Found');
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchPromotionalBannerData();
+  // }, []);
+  // return { isLoading, promotionalBannerData, errorMessage };
   const { isLoading, setIsLoading, errorMessage, setErrMessage }: any = useHandleStateUpdate();
   const { SUMMIT_APP_CONFIG }: any = CONSTANTS;
+  const currency_state_from_redux: any = useSelector(currency_selector_state);
+  const tokenFromStore: any = useSelector(get_access_token);
+  const [selectedCurrencyVal, setSelectedVal] = useState();
+  const [promotionalBannerData, setPromotionalBannerData] = useState<any>([]);
+
+  useEffect(() => {
+    setSelectedVal(currency_state_from_redux?.selected_currency_value);
+  }, [currency_state_from_redux]);
   const fetchPromotionalBannerData = async () => {
     let getPromotionalBannerData: any;
     setIsLoading(true);
+    const fields = ['title', 'image', 'description', 'sequence'];
     try {
-      getPromotionalBannerData = await promotionalBannerData(SUMMIT_APP_CONFIG, tokenFromStore?.token);
-      if (getPromotionalBannerData?.status === 200 && getPromotionalBannerData?.data?.msg === 'success') {
+      getPromotionalBannerData = await promotionalBannerAPI(SUMMIT_APP_CONFIG, fields, tokenFromStore?.token);
+      if (getPromotionalBannerData?.status === 200) {
         setPromotionalBannerData(getPromotionalBannerData?.data?.data);
       } else {
         setErrMessage('No Data Found');
@@ -24,11 +58,10 @@ const usePromotionalBanner = () => {
       setIsLoading(false);
     }
   };
-
   useEffect(() => {
     fetchPromotionalBannerData();
   }, []);
-  return { isLoading, promotionalBannerData, errorMessage };
+  return { promotionalBannerData, isLoading, errorMessage };
 };
 
 export default usePromotionalBanner;
--- services/api/home-page-apis/promotional-banner-api.ts
+++ services/api/home-page-apis/promotional-banner-api.ts
@@ -1,17 +1,8 @@
 import APP_CONFIG from '../../../interfaces/app-config-interface';
 import { executeGETAPI } from '../../../utils/http-methods';
-
-const promotionalBannerAPI = async (appConfig: APP_CONFIG, token: any) => {
-  const additionalParams = {}; // Add additional parameters if needed
-  // Use executeGETAPI to handle GET Request logic
-  const response = await executeGETAPI(
-    appConfig,
-    'promotional-banner-api',
-    token,
-    additionalParams // Pass additional parameters if needed
-  );
-
+const promotionalBannerAPI = async (appConfig: APP_CONFIG, reqParams: any, token: any) => {
+  const additionalParams = { fields: JSON.stringify(reqParams) };
+  const response = await executeGETAPI(undefined, '', token, additionalParams, `/api/resource/Promotional Banner`);
   return response;
 };
-
 export default promotionalBannerAPI;
