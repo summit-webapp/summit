178ca960d3fa241dd11a83465ae7bbb8961b7b05
karan <karan@8848digital.com>
2025-05-01 18:48:33 +0530

    config: :wrench: emr registry updated

--- utils/api-sdk-registry/emr_api_sdk_registry.ts
+++ utils/api-sdk-registry/emr_api_sdk_registry.ts
@@ -4,6 +4,7 @@ const emrAPISDKRegistry: any = {
   'post-insert-current-session-filters-api': '/api/insertCsFltrs',
   'get-product-listing-filters-api': '/api/getCatalogueFilterMasters',
   'get-product-list-api': '/api/getCatalogues',
+  'delete-current-session-filters-api': '/api/delOrdDsg',
   'post-move-current-session-api': '/api/moveDsg',
 };
 
