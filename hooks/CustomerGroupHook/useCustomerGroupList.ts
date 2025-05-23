396005af81f0a9bb67749ef03e36568a156fd7de
seemachal-8848 <167971880+seemachal-8848@users.noreply.github.com>
2025-05-23 11:41:54 +0530

    Update useCustomerGroupList.ts

--- hooks/CustomerGroupHook/useCustomerGroupList.ts
+++ hooks/CustomerGroupHook/useCustomerGroupList.ts
@@ -16,10 +16,10 @@ const useCustomerGroupList = () => {
     setIsLoading(true);
 
     /**
-     * Fetches user details from the API using the given token.
+     * Fetches customer group list from the API using the given token.
      *
      * @async
-     * @function getUserDetailsAPI
+     * @function fetchCustomerGroupList
      * @param {Object} SUMMIT_APP_CONFIG - The Summit API SDK object used to interact with the API.
      * @param {string} token - The authentication token obtained from the store.
      * @returns {Promise<void>} - Resolves when the API response is handled.
