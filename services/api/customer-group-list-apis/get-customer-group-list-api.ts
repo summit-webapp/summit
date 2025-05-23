4f2a45797e612f83f0caab56a3e3f089231d7b1c
seemachal-8848 <167971880+seemachal-8848@users.noreply.github.com>
2025-05-23 11:44:23 +0530

    Update get-customer-group-list-api.ts

--- services/api/customer-group-list-apis/get-customer-group-list-api.ts
+++ services/api/customer-group-list-apis/get-customer-group-list-api.ts
@@ -2,10 +2,10 @@ import APP_CONFIG from '../../../interfaces/app-config-interface';
 import { executeGETAPI } from '../../../utils/http-methods';
 
 /**
- * Fetches User Details from the API using the given parameters.
+ * Fetches customer group list from the API using the given parameters.
  *
  * @async
- * @function getUserDetailsAPI
+ * @function getCustomerGroupListAPI
  * @param {string} appConfig - The configuration of the application.
  * @param {string} token - The authentication token.
  * @returns {Promise<any>} - The response from the API call.
