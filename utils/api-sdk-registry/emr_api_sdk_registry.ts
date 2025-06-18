7b4207677818b003eb305880de2cc3b4013819f5
Navneet <navneet@8848digital.com>
2025-06-18 10:48:51 +0530

    feat: add get product design config api in emr_api_sdk_registry

--- utils/api-sdk-registry/emr_api_sdk_registry.ts
+++ utils/api-sdk-registry/emr_api_sdk_registry.ts
@@ -16,8 +16,8 @@ const emrAPISDKRegistry: any = {
   'place-order-api': '/api/createOrder',
   'price-list-api': '/api/fetchPrice',
   'get-site-map': '/api/collection-urls',
-  'get-product-design-y-options': '/api/getyDsgConfg',
   'refresh-price-api': '/api/refreshPrice'
+  'get-product-design-y-options': '/api/getyDsgConfg'
 };
 
 export default emrAPISDKRegistry;
