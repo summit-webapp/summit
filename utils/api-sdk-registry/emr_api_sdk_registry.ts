5958c49f20f794f88193281fa5ebd8a4ce464601
Navneet <navneet@8848digital.com>
2025-06-20 15:51:13 +0530

    feat: minor fix

--- utils/api-sdk-registry/emr_api_sdk_registry.ts
+++ utils/api-sdk-registry/emr_api_sdk_registry.ts
@@ -16,7 +16,7 @@ const emrAPISDKRegistry: any = {
   'place-order-api': '/api/createOrder',
   'price-list-api': '/api/fetchPrice',
   'get-site-map': '/api/collection-urls',
-  'refresh-price-api': '/api/refreshPrice'
+  'refresh-price-api': '/api/refreshPrice',
   'get-product-design-y-options': '/api/getyDsgConfg'
 };
 
