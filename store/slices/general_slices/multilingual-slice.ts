0339ac331e57fae54d52e7a403c5a5635dc8cfec
Navneet <navneet@8848digital.com>
2025-07-22 15:37:56 +0530

    feat: fix multilingual slice

--- store/slices/general_slices/multilingual-slice.ts
+++ store/slices/general_slices/multilingual-slice.ts
@@ -29,29 +29,3 @@ export const SelectedLang: any = createSlice({
 export const SelectedLangFromStore = (state: RootState) => state.SelectedLangDataReducer;
 export const { setLanguage } = SelectedLang.actions;
 export default SelectedLang.reducer;
-import { createSlice } from '@reduxjs/toolkit';
-import { RootState } from '../../root-reducer';
-
-interface selectedLang {
-  selectedLanguage: string;
-  error: string;
-}
-
-const initialState: selectedLang = {
-  selectedLanguage: 'en',
-  error: '',
-};
-
-export const SelectedLang: any = createSlice({
-  name: 'language',
-  initialState,
-  reducers: {
-    setLanguage(state, action) {
-      state.selectedLanguage = action.payload;
-    },
-  },
-});
-
-export const SelectedLangFromStore = (state: RootState) => state.SelectedLangDataReducer;
-export const { setLanguage } = SelectedLang.actions;
-export default SelectedLang.reducer;
