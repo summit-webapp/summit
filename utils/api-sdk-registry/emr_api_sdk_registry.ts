a6b498e07a3e2a0c2e67c1467c35694a4722ce94
nvnt122 <navneet@8848digital.com>
2025-08-20 22:47:37 +0530

    feat: add apis

--- utils/api-sdk-registry/emr_api_sdk_registry.ts
+++ utils/api-sdk-registry/emr_api_sdk_registry.ts
@@ -25,8 +25,10 @@ const emrAPISDKRegistry: any = {
   'sign-up-api': '/api/SignUp',
   'order-list-api': '/api/getListViewData',
   'update-user-preferences': '/api/storeUserPreferences',
+  'bom-details-api': '/api/bomChange',
   'get-metal-rate-api': '/api/getMetalRT',
   'post-metal-rate-api': '/api/updateMetalRT',
+  'get-sizes-api': '/api/getSizes',
 };
 
 export default emrAPISDKRegistry;
