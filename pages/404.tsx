77567719efceb88c6816d2d6c904b4e9c7e9ebd7
karan <karan@8848digital.com>
2026-03-09 11:59:00 +0530

    chore: complete rewrite of summit frontend, removed all components and business logic files

--- pages/404.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Pagenotfound from '../components/PageNotFound';
-
-const PageNotFoundPage = () => {
-  return <Pagenotfound />;
-};
-
-export default PageNotFoundPage;
