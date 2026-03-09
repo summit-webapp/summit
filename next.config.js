f4277b79c92ffee05598542b4e709666e6aede7d
karan <karan@8848digital.com>
2026-03-09 15:11:26 +0530

    chore: remove bundle analyzer configuration from Next.js setup

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 const nextConfig = {
-  images: {
-    domains: ['summit.8848digitalerp.com', 'staging-twinkle.8848digitalerp.com', 'emr-euro-shine.8848digitalerp.com', 'backend.atelier-reya.com', 'kc-backend.8848digitalcloud.com'],
-  },    
-    reactStrictMode: false,
+  reactStrictMode: false,
   swcMinify: true,
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
-// module.exports = nextConfig;
+module.exports = nextConfig;
